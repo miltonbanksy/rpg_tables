f94c04d2cec902a27e0b47bc441c28a7862da544
miltonbanksy <miltonbanksy@gmail.com>
2026-03-25 00:45:22 +0900

    added settlements list

---
+++ settlement_names.js
@@ -0,0 +1,25 @@
+const settlement_names_1 = [
+    "crow","grave","green","moon","night","pot","wood"
+];
+
+const settlement_names_2 = [
+    "crow","green","night","pot","potion","wood"
+];
+
+const settlement_names_uk = [
+    "amp",
+    "beach","beacon","bed","black","bod","bourn","brack","bridge","bright","buck","bury",
+    "cam","chest","chester","combe","cong","congle","crew",
+    "don","dun",
+    "fen","field","fold","font","ford",
+    "ham","hill","hove","hunt","hurst",
+    "land",
+    "marlowe","mouth",
+    "nan","nell","nut",
+    "pool",
+    "ram","rest",
+    "sand","sea","shire","shore","stable","stoke",
+    "ting","toll","ton","turn",
+    "vale",
+    "wich"
+]
